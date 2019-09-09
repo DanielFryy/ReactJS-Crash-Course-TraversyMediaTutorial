27b7a3cc286b393d0ae31376bb7f9d9937d886c8
DanielFryy <dgz1606@gmail.com>
2019-09-09 00:57:44 -0500

    Completion of tutorial with addition of missing PropTypes

--- react_crash_todo/src/components/AddTodo/AddTodo.js
+++ react_crash_todo/src/components/AddTodo/AddTodo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class AddTodo extends Component {
   
@@ -25,5 +26,10 @@ class AddTodo extends Component {
     );
   }
 }
+
+// PropTypes
+AddTodo.propTypes = {
+  addTodo: PropTypes.func.isRequired
+};
 
 export default AddTodo;
